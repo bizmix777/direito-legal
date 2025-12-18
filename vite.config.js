f7c0bdb18e7fa0e5a0ffd51f2ad89574929a5fdf
bizmix777 <bizmix777@gmail.com>
2025-12-18 10:06:08 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: '/direito-legal/', // Caminho fixo, sem ponto!
+  # base: '/direito-legal/', // Caminho fixo, sem ponto! #
   server: {
     host: "::",
     port: 8080,
